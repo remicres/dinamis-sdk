b29540fc87997522118d5325f6668dd837f8a929
Remi Cresson <remi.cresson@inrae.fr>
2023-05-01 22:25:35 +0200

    Add debug msgs and avoid signing empty url list

--- dinamis_sdk/s3.py
+++ dinamis_sdk/s3.py
@@ -481,7 +481,8 @@ def get_signed_urls(
             raise ValueError(
                 f"No signed url batch found in response: {response.json()}"
             )
-        assert all(key in signed_url_batch for key in not_signed_urls)
+        assert all(key in signed_url_batch.hrefs.items()
+                   for key in not_signed_urls)
         for url, href in signed_url_batch.hrefs.items():
             signed_url = SignedURL(expiry=signed_url_batch.expiry, href=href)
             CACHE[url] = signed_url
