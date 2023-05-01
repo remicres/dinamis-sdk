4d3819e1c0b67c5f96c93fb79a85fc8388b342cd
Remi Cresson <remi.cresson@inrae.fr>
2023-05-01 22:14:19 +0200

    Add debug msgs and avoid signing empty url list

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name="dinamis-sdk",
-    version="0.0.7",
+    version="0.0.7a",
     description="DINAMIS SDK",
     python_requires=">=3.8",
     author="Remi Cresson",
