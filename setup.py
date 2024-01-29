d82ae5958a7e3fcfe5e324ce668b91e6dea001fc
Cresson Remi <remi.cresson@irstea.fr>
2024-01-29 12:57:05 +0100

    Update 2 files
    - /dinamis_sdk/utils.py
    - /setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name="dinamis-sdk",
-    version="0.0.15",
+    version="0.0.16",
     description="DINAMIS SDK",
     python_requires=">=3.8",
     author="Remi Cresson",
