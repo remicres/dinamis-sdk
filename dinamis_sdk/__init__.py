cad165e412b1b6e4fad2d38162420122168c651f
Cresson Remi <remi.cresson@irstea.fr>
2024-05-21 19:45:55 +0200

    Update file __init__.py

--- dinamis_sdk/__init__.py
+++ dinamis_sdk/__init__.py
@@ -1,5 +1,13 @@
 """Dinamis SDK module."""
 import pkg_resources
 __version__ = pkg_resources.require("dinamis-sdk")[0].version
-from dinamis_sdk.s3 import sign, sign_inplace  # noqa
+from dinamis_sdk.s3 import (
+    sign,
+    sign_inplace,
+    sign_url,
+    sign_item,
+    sign_assets,
+    sign_asset,
+    sign_item_collection
+)  # noqa
 from dinamis_sdk import auth  # noqa
