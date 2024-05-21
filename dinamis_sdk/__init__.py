dd0918bc7b36cd287330d4c449c56098402a0f90
Cresson Remi <remi.cresson@irstea.fr>
2024-05-21 16:38:25 +0200

    add: sign() in __init__

--- dinamis_sdk/__init__.py
+++ dinamis_sdk/__init__.py
@@ -1,5 +1,5 @@
 """Dinamis SDK module."""
 import pkg_resources
 __version__ = pkg_resources.require("dinamis-sdk")[0].version
-from dinamis_sdk.s3 import sign_inplace  # noqa
+from dinamis_sdk.s3 import sign, sign_inplace  # noqa
 from dinamis_sdk import auth  # noqa
