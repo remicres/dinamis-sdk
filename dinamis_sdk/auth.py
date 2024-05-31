25ce727d44dc00a2901e5e4deb967955b3a1ceaa
Cresson Remi <remi.cresson@irstea.fr>
2024-05-31 11:06:50 +0200

    Update 2 files
    - /setup.py
    - /dinamis_sdk/auth.py

--- dinamis_sdk/auth.py
+++ dinamis_sdk/auth.py
@@ -59,7 +59,7 @@ class GrantMethodBase:
         """Base payload."""
         return {
             "client_id": self.client_id,
-            "scope": "offline_access openid"
+            "scope": "openid"
         }
 
     def refresh_token(self, old_jwt: JWT) -> JWT:
